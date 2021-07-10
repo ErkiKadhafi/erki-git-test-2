55a4699a5585ddb98b8f154e21786fc5099d0484
ErkiKadhafi <erki.qaedhafi@gmail.com>
2021-07-10 11:36:27 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -11,5 +11,10 @@
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
          Minus, ea iste? Voluptatum dicta magni voluptatem excepturi placeat rerum est iure! Tempore, natus iste. 
          Doloribus dolorum quasi ullam odit, accusamus, alias id deserunt amet eveniet ad voluptate nemo qui soluta ipsam!</p>
+    <ul>
+        <li>Ini list 1</li>
+        <li>Ini list 2</li>
+        <li>Ini list 3</li>
+    </ul>
 </body>
 </html>
